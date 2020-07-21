f65a1131a77c9ac9c29f0fede20f50660b8073b1
parv3213 <parv3213@gmail.com>
2020-07-21 10:20:37 +0530

    added vote function and tests

--- src/contracts/Election.sol
+++ src/contracts/Election.sol
@@ -8,6 +8,7 @@ contract Election {
     }
     uint256 public candidatesCount;
     mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
     constructor() public {
         addCandidate("Candidate 1");
@@ -17,5 +18,15 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        require(
+            _candidateId <= candidatesCount && _candidateId > 0,
+            "Invalid candidate"
+        );
+        require(!voters[msg.sender], "Vote must not have voted before.");
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
--- test/ElectionTest.test.js
+++ test/ElectionTest.test.js
@@ -3,7 +3,7 @@ const { assert } = require("chai");
 const Election = artifacts.require("Election");
 
 contract(Election, (accounts) => {
-	const [owner, voter] = accounts;
+	const [owner, voter1, voter2] = accounts;
 	let election;
 	before(async () => {
 		election = await Election.new();
@@ -20,6 +20,49 @@ contract(Election, (accounts) => {
 			assert.equal(candidate2.id.toNumber(), 2);
 			assert.equal(candidate2.name, "Candidate 2");
 			assert.equal(candidate2.voteCount.toNumber(), 0);
+		});
+	});
+	describe("Vote function", () => {
+		it("Voter gets registered", async () => {
+			let voterRegistered = await election.voters(voter1);
+			assert.equal(voterRegistered, false);
+			await election.vote(1, { from: voter1 });
+			voterRegistered = await election.voters(voter1);
+			assert.equal(voterRegistered, true);
+		});
+		it("Voter can vote only once", async () => {
+			try {
+				await election.vote(1, { from: voter1 });
+				assert.fail();
+			} catch (e) {
+				assert(e.message.indexOf("revert") !== -1);
+			}
+			try {
+				await election.vote(2, { from: voter1 });
+				assert.fail();
+			} catch (e) {
+				assert(e.message.indexOf("revert") !== -1);
+			}
+		});
+		it("Vote count of candidate increasing", async () => {
+			const candidate1VoteCount = await election.candidates(1);
+			assert.equal(candidate1VoteCount.voteCount.toNumber(), 1);
+			const candidate2VoteCount = await election.candidates(2);
+			assert.equal(candidate2VoteCount.voteCount.toNumber(), 0);
+		});
+		it("Revert for invalid candidate", async () => {
+			try {
+				await election.vote(3, { from: voter2 });
+				assert.fail();
+			} catch (e) {
+				assert(e.message.indexOf("revert") !== -1);
+			}
+			try {
+				await election.vote(-10, { from: voter2 });
+				assert.fail();
+			} catch (e) {
+				assert(e.message.indexOf("revert") !== -1);
+			}
 		});
 	});
 });
